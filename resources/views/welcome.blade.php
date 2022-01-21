5cf26676aa4960faf80993a1fe0baeed4dbe4fc3
akash.y <akash.y@media.net>
2022-01-21 19:07:07 +0530

    learned how to use external css and images

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,8 @@
 @section("content")
 <div class="flex-center position-ref full-height">
     <div class="content">
+        <img src="/img/pizza-house.png" alt="pizza house logo">
         <div class="title m-b-md">
-            Pizza House<br />
             The North's Best Pizzas
         </div>
     </div>
