99cf1b64e71951b4faf01b934cea973c38222896
akash.y <akash.y@media.net>
2022-01-24 13:10:41 +0530

    sass

--- resources/views/pizzas.blade.php
+++ resources/views/pizzas.blade.php
@@ -1,27 +1,15 @@
-<?php
-    /*
-    if we have common code on all the pages like header and footer will be same on different pages
-    if any changes is require on header or footer than we have to change it on all the pages which is very hectic and time consuming
-    so to avoid that we can place that part of code in on file and than use them everywhere
-    we can use @extends to get the which layout we want to use  & here layouts. layout is like layouts/layout here we have used . instead of
-    three main thing is here @extends, @section, @yield for yield check layout file
-    */
-?>
-@extends("layouts.layout")
+@extends('layouts.layout')
 
-<!-- 'content' is name of this section so you can write any name you want -->
-@section("content")
-<div class="flex-center position-ref full-height">
-    <div class="content">
-        <div class="title m-b-md">
-            Pizza List
-        </div>
-        @foreach($pizzas as $pizza)
-        <div>
-            <!--    both are printing the values so bcause Model gives the object that's why use -> but the second also working fine        -->
-            {{ $pizza->name }} - {{ $pizza['type'] }} - {{ $pizza['base'] }}
-        </div>
-        @endforeach
+@section('content')
+<div class="wrapper pizza-index">
+    <h1>Pizza Orders</h1>
+    @foreach($pizzas as $pizza)
+    <div class="pizza-item">
+        <img src="/img/pizza.png" alt="pizza icon">
+        <h4><a href="/pizzas/{{ $pizza->id }}">{{ $pizza->name }}</a></h4>
     </div>
+    @endforeach
+
 </div>
+
 @endsection
